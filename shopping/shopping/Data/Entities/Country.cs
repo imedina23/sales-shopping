69b763d83ab250cb263dc11b17d4b98c9d6daba9
idy <riosidy3@gmail.com>
2022-03-17 17:45:02 -0500

    created db entity - added country table

---
+++ shopping/shopping/Data/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shopping.Data.Entities
+{
+    public class Country
+    {
+        
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Field {0} is required")]
+        [MaxLength(50, ErrorMessage ="field {0} mysbe maximum {1} characters")]
+        [Display(Name ="Country")]
+        public string Name { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
---
+++ shopping/shopping/Migrations/20220317222633_initialDb.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shopping.Migrations
+{
+    public partial class initialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "countries",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    MyProperty = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_countries", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "countries");
+        }
+    }
+}
